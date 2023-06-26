685be0e6c06ac6efefa27edeabee59bfed3b665a
Andrii Dehtiarov <Andrii.Dehtiarov@iwgplc.com>
2023-06-26 11:11:49 +0300

    Added comments to explain validation process in yaml deserializer

--- src/Nox.Solution/Resolvers/NoxYamlSerializer.cs
+++ src/Nox.Solution/Resolvers/NoxYamlSerializer.cs
@@ -17,6 +17,17 @@ namespace Nox.Solution.Resolvers;
 /// </summary>
 internal static class NoxYamlSerializer
 {
+    /// <summary>
+    /// Deserialize yaml content to T object.
+    /// It deserilizes data to string then serialize to json object to validate all properties against data annotation schema.
+    /// It should validate twice: first time it considers yaml content as json object to find missed required fields.
+    /// If object validation carries out then consider yaml content as certain T type to find corectness of properties, whether properties
+    /// match json content.
+    /// </summary>
+    /// <typeparam name="T">Any type corresponds to yaml content.</typeparam>
+    /// <param name="yaml">Yaml file string content.</param>
+    /// <returns>Deserialized T instance.</returns>
+    /// <exception cref="NoxSolutionConfigurationException">Exception bring error messages caught during validation.</exception>
     public static T? Deserialize<T>(string yaml)
     {
         var deserializer = new DeserializerBuilder()
@@ -70,7 +81,8 @@ internal static class NoxYamlSerializer
 
         if (errors.Count > 0)
         {
-            throw new NoxSolutionConfigurationException(string.Join("\n", errors.Distinct()));
+            var message = string.Join("\n", errors.Distinct());
+            throw new NoxSolutionConfigurationException(message);
         }
 
         return yamlTypedObjectInstance;
